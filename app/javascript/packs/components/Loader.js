2ff4882f8ec8c81cc67e05089a49adb557bfc403
Paul Cardoos <Paul.Cardoos001@umb.edu>
2021-03-11 12:16:37 -0500

    edited loading model

--- app/javascript/packs/components/Loader.js
+++ app/javascript/packs/components/Loader.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { Row , Col, Container} from 'react-bootstrap'
 
 
 const Loader = () => {
     return (
+      <Container>
+        <Row>
+        <Col >
+        
         <div className="mt-5 d-flex justify-content-center">
           <div className="spinner-border" role="status">
             <span className="sr-only">Model is Loading</span>
          </div>
       </div>
 
+            <h2 className='text-center mt-3'>Model is Loading ... </h2>
+        </Col>  
+        </Row>
+      </Container>
             
     )
 }
